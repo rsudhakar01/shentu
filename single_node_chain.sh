c9dbdd311474c6a512de57c30c350a6915997d86
ziyi-zhang-1130 <ziyi.zhang@certik.org>
2020-10-31 15:31:45 -0400

    Update single_node_chain.sh

--- single_node_chain.sh
+++ single_node_chain.sh
@@ -7,12 +7,11 @@ rm -rf ~/.certikd
 rm -rf ~/.certikcli
 
 certikd init node0 --chain-id certikchain
-certikcli config chain-id certikchain
 certikcli config keyring-backend test
 
 certikcli keys add jack
 jack=$(certikcli keys show jack -a)
-certikd add-genesis-account $jack 1000000000uctk --vesting-amount=200000000uctk --period=10 --num-periods=5
+certikd add-genesis-account $jack 1000000000uctk
 
 certikd gentx --name jack --amount 2000000uctk --keyring-backend test
 certikd collect-gentxs
