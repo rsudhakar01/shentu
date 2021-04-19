849fcbcaeb4d47f3fe2ecc919b5d241bc1fb7aba
Ziyi Zhang <66639536+ziyi-zhang-1130@users.noreply.github.com>
2021-04-19 17:18:32 -0400

    Update txs.sh

--- txs.sh
+++ txs.sh
@@ -8,32 +8,6 @@ echo
 mary=$(certik keys show mary -a)
 echo
 
-certik tx cert issue-certificate COMPILATION CCC --compiler A --bytecode-hash B --from $jack --chain-id certikchain -y
-sleep 5
-echo
+jack_pubkey=$(certik tendermint show-validator)
 
-certik tx cert issue-certificate AUDITING ccccc --from $mary --chain-id certikchain -y
-sleep 5
-echo
-
-<<COMMENT1
-certik tx cert issue-certificate PROOF ccccc --from $jack --chain-id certikchain -y
-sleep 5
-echo
-
-certik tx cert issue-certificate ORACLEOPERATOR ccccc --from $jack --chain-id certikchain -y
-sleep 5
-echo
-
-certik tx cert issue-certificate SHIELDPOOLCREATOR ccccc --from $jack --chain-id certikchain -y
-sleep 5
-echo
-
-certik tx cert issue-certificate IDENTITY ccccc --from $jack --chain-id certikchain -y
-sleep 5
-echo
-
-certik tx cert issue-certificate GENERAL ccccc --from $jack --chain-id certikchain -y
-sleep 5
-echo
-COMMENT1
+certik tx cert certify-platform $jack_pubkey test-platform --from $jack --chain-id certikchain -y
