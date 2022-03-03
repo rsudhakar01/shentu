e2466ff17cb901f1dbe69a910a041a85a11afe29
Ziyi Zhang <66639536+ziyi-zhang-1130@users.noreply.github.com>
2022-03-03 15:34:44 -0500

    unlock proto

--- x/interview/types/tx.pb.go
+++ x/interview/types/tx.pb.go
@@ -103,9 +103,86 @@ func (m *MsgLockUserResponse) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_MsgLockUserResponse proto.InternalMessageInfo
 
+// MsgCreatePool defines the attributes of a create-pool transaction.
+type MsgUnlockUser struct {
+	From string `protobuf:"bytes,1,opt,name=from,proto3" json:"from,omitempty" yaml:"from"`
+	Id   uint64 `protobuf:"varint,2,opt,name=id,proto3" json:"id,omitempty" yaml:"id"`
+}
+
+func (m *MsgUnlockUser) Reset()         { *m = MsgUnlockUser{} }
+func (m *MsgUnlockUser) String() string { return proto.CompactTextString(m) }
+func (*MsgUnlockUser) ProtoMessage()    {}
+func (*MsgUnlockUser) Descriptor() ([]byte, []int) {
+	return fileDescriptor_8ed4aaa105dd2890, []int{2}
+}
+func (m *MsgUnlockUser) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *MsgUnlockUser) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_MsgUnlockUser.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *MsgUnlockUser) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_MsgUnlockUser.Merge(m, src)
+}
+func (m *MsgUnlockUser) XXX_Size() int {
+	return m.Size()
+}
+func (m *MsgUnlockUser) XXX_DiscardUnknown() {
+	xxx_messageInfo_MsgUnlockUser.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_MsgUnlockUser proto.InternalMessageInfo
+
+type MsgUnlockUserResponse struct {
+}
+
+func (m *MsgUnlockUserResponse) Reset()         { *m = MsgUnlockUserResponse{} }
+func (m *MsgUnlockUserResponse) String() string { return proto.CompactTextString(m) }
+func (*MsgUnlockUserResponse) ProtoMessage()    {}
+func (*MsgUnlockUserResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_8ed4aaa105dd2890, []int{3}
+}
+func (m *MsgUnlockUserResponse) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *MsgUnlockUserResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_MsgUnlockUserResponse.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *MsgUnlockUserResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_MsgUnlockUserResponse.Merge(m, src)
+}
+func (m *MsgUnlockUserResponse) XXX_Size() int {
+	return m.Size()
+}
+func (m *MsgUnlockUserResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_MsgUnlockUserResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_MsgUnlockUserResponse proto.InternalMessageInfo
+
 func init() {
 	proto.RegisterType((*MsgLockUser)(nil), "shentu.interview.v1alpha1.MsgLockUser")
 	proto.RegisterType((*MsgLockUserResponse)(nil), "shentu.interview.v1alpha1.MsgLockUserResponse")
+	proto.RegisterType((*MsgUnlockUser)(nil), "shentu.interview.v1alpha1.MsgUnlockUser")
+	proto.RegisterType((*MsgUnlockUserResponse)(nil), "shentu.interview.v1alpha1.MsgUnlockUserResponse")
 }
 
 func init() {
@@ -113,7 +190,7 @@ func init() {
 }
 
 var fileDescriptor_8ed4aaa105dd2890 = []byte{
-	// 283 bytes of a gzipped FileDescriptorProto
+	// 303 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x52, 0x2a, 0xce, 0x48, 0xcd,
 	0x2b, 0x29, 0xd5, 0xcf, 0xcc, 0x2b, 0x49, 0x2d, 0x2a, 0xcb, 0x4c, 0x2d, 0xd7, 0x2f, 0x33, 0x4c,
 	0xcc, 0x29, 0xc8, 0x48, 0x34, 0xd4, 0x2f, 0xa9, 0xd0, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x92,
@@ -124,14 +201,15 @@ var fileDescriptor_8ed4aaa105dd2890 = []byte{
 	0x2a, 0x05, 0x81, 0x25, 0x85, 0x64, 0xb9, 0x98, 0x32, 0x53, 0x24, 0x98, 0x14, 0x18, 0x35, 0x58,
 	0x9c, 0x78, 0x3f, 0xdd, 0x93, 0xe7, 0x84, 0x28, 0xc9, 0x4c, 0x51, 0x0a, 0x62, 0xca, 0x4c, 0xb1,
 	0xe2, 0xe8, 0x58, 0x20, 0xcf, 0xf0, 0x62, 0x81, 0x3c, 0x83, 0x92, 0x28, 0x97, 0x30, 0x92, 0xe1,
-	0x41, 0xa9, 0xc5, 0x05, 0xf9, 0x79, 0xc5, 0xa9, 0x46, 0x99, 0x5c, 0xcc, 0xbe, 0xc5, 0xe9, 0x42,
-	0x49, 0x5c, 0x1c, 0x70, 0x7b, 0xd5, 0xf4, 0x70, 0x3a, 0x5c, 0x0f, 0xc9, 0x08, 0x29, 0x3d, 0xe2,
-	0xd4, 0xc1, 0xac, 0x72, 0xf2, 0x3b, 0xf1, 0x48, 0x8e, 0xf1, 0xc2, 0x23, 0x39, 0xc6, 0x07, 0x8f,
-	0xe4, 0x18, 0x27, 0x3c, 0x96, 0x63, 0xb8, 0xf0, 0x58, 0x8e, 0xe1, 0xc6, 0x63, 0x39, 0x86, 0x28,
-	0x93, 0xf4, 0xcc, 0x92, 0x8c, 0xd2, 0x24, 0xbd, 0xe4, 0xfc, 0x5c, 0xfd, 0xe4, 0xd4, 0xa2, 0x92,
-	0xcc, 0xec, 0xb4, 0xfc, 0xd2, 0xbc, 0x94, 0xc4, 0x92, 0xcc, 0xfc, 0x3c, 0x7d, 0x68, 0x48, 0x57,
-	0x20, 0x85, 0x75, 0x49, 0x65, 0x41, 0x6a, 0x71, 0x12, 0x1b, 0x38, 0xd4, 0x8c, 0x01, 0x01, 0x00,
-	0x00, 0xff, 0xff, 0x23, 0x58, 0x18, 0x28, 0x8c, 0x01, 0x00, 0x00,
+	0x41, 0xa9, 0xc5, 0x05, 0xf9, 0x79, 0xc5, 0xa9, 0x4a, 0xb1, 0x5c, 0xbc, 0xbe, 0xc5, 0xe9, 0xa1,
+	0x79, 0x39, 0xb4, 0xb1, 0x55, 0x9c, 0x4b, 0x14, 0xc5, 0x78, 0x98, 0xbd, 0x46, 0x99, 0x5c, 0xcc,
+	0xbe, 0xc5, 0xe9, 0x42, 0x49, 0x5c, 0x1c, 0x70, 0xff, 0xaa, 0xe9, 0xe1, 0x0c, 0x30, 0x3d, 0x24,
+	0xa7, 0x4b, 0xe9, 0x11, 0xa7, 0x0e, 0x66, 0x95, 0x93, 0xdf, 0x89, 0x47, 0x72, 0x8c, 0x17, 0x1e,
+	0xc9, 0x31, 0x3e, 0x78, 0x24, 0xc7, 0x38, 0xe1, 0xb1, 0x1c, 0xc3, 0x85, 0xc7, 0x72, 0x0c, 0x37,
+	0x1e, 0xcb, 0x31, 0x44, 0x99, 0xa4, 0x67, 0x96, 0x64, 0x94, 0x26, 0xe9, 0x25, 0xe7, 0xe7, 0xea,
+	0x27, 0xa7, 0x16, 0x95, 0x64, 0x66, 0xa7, 0xe5, 0x97, 0xe6, 0xa5, 0x24, 0x96, 0x64, 0xe6, 0xe7,
+	0xe9, 0x43, 0x63, 0xb8, 0x02, 0x29, 0x8e, 0x4b, 0x2a, 0x0b, 0x52, 0x8b, 0x93, 0xd8, 0xc0, 0xb1,
+	0x65, 0x0c, 0x08, 0x00, 0x00, 0xff, 0xff, 0x68, 0x00, 0x19, 0x3f, 0x04, 0x02, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -272,6 +350,64 @@ func (m *MsgLockUserResponse) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	return len(dAtA) - i, nil
 }
 
+func (m *MsgUnlockUser) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *MsgUnlockUser) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *MsgUnlockUser) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if m.Id != 0 {
+		i = encodeVarintTx(dAtA, i, uint64(m.Id))
+		i--
+		dAtA[i] = 0x10
+	}
+	if len(m.From) > 0 {
+		i -= len(m.From)
+		copy(dAtA[i:], m.From)
+		i = encodeVarintTx(dAtA, i, uint64(len(m.From)))
+		i--
+		dAtA[i] = 0xa
+	}
+	return len(dAtA) - i, nil
+}
+
+func (m *MsgUnlockUserResponse) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *MsgUnlockUserResponse) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *MsgUnlockUserResponse) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	return len(dAtA) - i, nil
+}
+
 func encodeVarintTx(dAtA []byte, offset int, v uint64) int {
 	offset -= sovTx(v)
 	base := offset
@@ -300,6 +436,31 @@ func (m *MsgLockUser) Size() (n int) {
 }
 
 func (m *MsgLockUserResponse) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	return n
+}
+
+func (m *MsgUnlockUser) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	l = len(m.From)
+	if l > 0 {
+		n += 1 + l + sovTx(uint64(l))
+	}
+	if m.Id != 0 {
+		n += 1 + sovTx(uint64(m.Id))
+	}
+	return n
+}
+
+func (m *MsgUnlockUserResponse) Size() (n int) {
 	if m == nil {
 		return 0
 	}
@@ -445,6 +606,163 @@ func (m *MsgLockUserResponse) Unmarshal(dAtA []byte) error {
 		}
 		if fieldNum <= 0 {
 			return fmt.Errorf("proto: MsgLockUserResponse: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		default:
+			iNdEx = preIndex
+			skippy, err := skipTx(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if skippy < 0 {
+				return ErrInvalidLengthTx
+			}
+			if (iNdEx + skippy) < 0 {
+				return ErrInvalidLengthTx
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *MsgUnlockUser) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowTx
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: MsgUnlockUser: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: MsgUnlockUser: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field From", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowTx
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthTx
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthTx
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.From = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		case 2:
+			if wireType != 0 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Id", wireType)
+			}
+			m.Id = 0
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowTx
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				m.Id |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+		default:
+			iNdEx = preIndex
+			skippy, err := skipTx(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if skippy < 0 {
+				return ErrInvalidLengthTx
+			}
+			if (iNdEx + skippy) < 0 {
+				return ErrInvalidLengthTx
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *MsgUnlockUserResponse) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowTx
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: MsgUnlockUserResponse: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: MsgUnlockUserResponse: illegal tag %d (wire type %d)", fieldNum, wire)
 		}
 		switch fieldNum {
 		default:
